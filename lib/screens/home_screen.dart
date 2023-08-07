acaa08b0e7e011282757545e7f06ba9dd95f7e87
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-08-06 20:50:36 -0500

    Revert "Merge branch 'master' of https://github.com/wtwente20/Flutter-Pokedex"
    This reverts commit d39e1b8c3b8e11ac7c592e12a49cf8b61327cf9e, reversing
    changes made to 8c8c4bc6822578a210e77b4b18748f2911479c31.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_app/models/pokemon.dart';
 import 'package:pokedex_app/repositories/pokemon_repository.dart';
-import 'package:pokedex_app/screens/add_pokemon_screen.dart';
 import 'package:pokedex_app/screens/pokemon_screen.dart';
-import 'package:pokedex_app/widgets/pokedex_drawer.dart';
 
 import '../data/regions.dart';
 
@@ -16,7 +14,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<List<Pokemon>> futurePokemons;
-  List<Pokemon> partyPokemons = [];
 
   @override
   void initState() {
@@ -29,118 +26,73 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pokedex'),
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: Image.asset(
-                'assets/images/287221.png'), // Path to your pokeball asset
-            onPressed: () => Scaffold.of(context).openDrawer(),
-          ),
-        ),
-      ),
-      drawer: FutureBuilder<List<Pokemon>>(
-        future: futurePokemons,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return PokedexDrawer(
-              allPokemons: snapshot.data!,
-              partyPokemons: partyPokemons,
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return Drawer(child: Center(child: CircularProgressIndicator()));
-          } else {
-            // You can add more specific handling based on error type or even allow retry
-            return Drawer(child: Center(child: Text('Failed to load data')));
-          }
-        },
       ),
       body: FutureBuilder<List<Pokemon>>(
-        future: futurePokemons,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final regions = groupPokemonsByRegion(snapshot.data!);
+  future: futurePokemons,
+  builder: (context, snapshot) {
+    if (snapshot.hasData) {
+      final regions = groupPokemonsByRegion(snapshot.data!);
 
-            return ListView.builder(
-              itemCount: regions.length,
-              itemBuilder: (context, regionIndex) {
-                final region = regions[regionIndex];
-                return ExpansionTile(
-                  title: Text(region.name),
-                  children: region.pokemons.map((pokemon) {
-                    final theme = Theme.of(context);
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: theme.cardTheme.color,
-                          onPrimary: theme.colorScheme.onPrimary,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            side: BorderSide(
-                                color: theme.colorScheme.primary, width: 1.0),
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  PokemonScreen(pokemon: pokemon),
-                            ),
-                          );
-                        },
-                        child: Row(
-                          children: [
-                            Text(
-                              '${pokemon.id}. ${pokemon.name}',
-                              style: TextStyle(
-                                color: theme.colorScheme.onBackground,
-                              ),
-                            ),
-                            Spacer(),
-                            Image.network(
-                              pokemon.imageUrl,
-                              height: 50,
-                              width: 50,
-                            ),
-                          ],
-                        ),
+      return ListView.builder(
+        itemCount: regions.length,
+        itemBuilder: (context, regionIndex) {
+          final region = regions[regionIndex];
+          return ExpansionTile(
+            title: Text(region.name),
+            children: region.pokemons.map((pokemon) {
+              final theme = Theme.of(context);
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: theme.cardTheme.color,
+                    onPrimary: theme.colorScheme.onPrimary,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(
+                          color: theme.colorScheme.primary, width: 1.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PokemonScreen(pokemon: pokemon),
                       ),
                     );
-                  }).toList(),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('${snapshot.error}'));
-          }
-
-          return Center(child: CircularProgressIndicator());
+                  },
+                  child: Row(
+                    children: [
+                      Text(
+                        '${pokemon.id}. ${pokemon.name}',
+                        style: TextStyle(
+                          color: theme.colorScheme.onBackground,
+                        ),
+                      ),
+                      Spacer(),
+                      Image.network(
+                        pokemon.imageUrl,
+                        height: 50,
+                        width: 50,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
+          );
         },
-      ),
-      floatingActionButton: Builder(
-        builder: (context) => FloatingActionButton(
-          onPressed: () {
-            _navigateToAddPokemonScreen(context);
-          },
-          child: Text('Add Pokemon to Party'),
-        ),
-      ),
-    );
-  }
-
-  void _navigateToAddPokemonScreen(BuildContext context) async {
-    final addedPokemon = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => AddPokemonScreen(allPokemons: partyPokemons),
-      ),
-    );
-
-    if (addedPokemon != null) {
-      setState(() {
-        partyPokemons.add(addedPokemon);
-      });
+      );
+    } else if (snapshot.hasError) {
+      return Text('${snapshot.error}');
     }
+
+    // By default, show a loading spinner.
+    return CircularProgressIndicator();
+  },
+),
+
+    );
   }
 }
